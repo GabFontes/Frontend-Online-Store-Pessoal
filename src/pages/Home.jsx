005e41b71609dff55dea3877ace28f6b33d6d43f
gab <gabrielfontes321@gmail.com>
2021-11-19 19:17:09 -0300

    d

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import ProductsList from '../components/ProductsList';
 import Sidebar from '../components/Sidebar';
+import * as api from '../services/api';
 
 export default class Home extends React.Component {
-  // constructor(){
-  //
+  constructor() {
+    super();
+
+    this.productsFetch = this.productsFetch.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      products: [],
+      query: '',
+      categoryId: '',
+    };
+  }
+
+  handleChange({ target: { value } }) {
+    this.setState({
+      query: value,
+    });
+  }
+
+  async productsFetch() {
+    const { categoryId, query } = this.state;
+    const products = await api.getProductsFromCategoryAndQuery(categoryId, query);
+    this.setState({
+      products,
+    });
+  }
 
   render() {
+    const { query, products: { results } } = this.state;
     return (
       <div>
         <h3 data-testid="home-initial-message">
@@ -14,11 +41,27 @@ export default class Home extends React.Component {
         </h3>
         <form>
           <label htmlFor="input-search">
-            <input type="text" />
+            <input
+              onChange={ this.handleChange }
+              value={ query }
+              data-testid="query-input"
+              type="text"
+            />
           </label>
+          <button
+            type="button"
+            data-testid="query-button"
+            onClick={ this.productsFetch }
+          >
+            Pesquisar
+          </button>
         </form>
         <Link data-testid="shopping-cart-button" to="/cart">Cart</Link>
         <Sidebar />
+        {results && results.map((result) => (<ProductsList
+          key={ result.id }
+          product={ result }
+        />))}
       </div>
     );
   }
